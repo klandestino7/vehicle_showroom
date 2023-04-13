0e21aacbf12daeef86eb0c0882cdb0ef43b12d84
klandestino7 <klandestino6@hotmail.com>
2023-04-13 11:07:01 -0300

    test: trying fix the preview bug on deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ import * as path from 'path'
 export default defineConfig({
   resolve: {
     alias: [
-      { find: '@', replacement: path.resolve(__dirname, 'src') }],
+      { find: '@', replacement: '/src' },
+    ]
   },
   plugins: [react()]
 })
